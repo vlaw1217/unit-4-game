ff38b77ab8123512806d12d1b0e9833f66a7d61d
Vicky Law <vicky.law1217@gmail.com>
2019-07-09 16:48:34 -0400

    add win and lose comments

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,98 +11,122 @@ $(document).ready(() => {
         $("#start").on("click", () => {
             let targetNum = [24, 46, 29, 44, 31, 37, 50, 18, 22, 33];
             let randomNum = Math.floor(Math.random() * (targetNum.length));
-             document.getElementById("fixNum").innerHTML = (targetNum[randomNum]);
+            document.getElementById("fixNum").innerHTML = (targetNum[randomNum]);
+
 
             randomHiddenNum = targetNum[randomNum];
-            
+
+
+
+
             let crystalHiddenNum = [4, 5, 6, 7, 8, 9];
-            
-            let hiddenNumber = Math.floor(Math.random() * (crystalHiddenNum.length));
-            x = crystalHiddenNum[hiddenNumber];
-            
+
+            /*let hiddenNumber = Math.floor(Math.random() * (crystalHiddenNum.length));
+            x = crystalHiddenNum[hiddenNumber];*/
+
             let hiddenNumberG = Math.floor(Math.random() * (crystalHiddenNum.length));
             g = crystalHiddenNum[hiddenNumberG];
-        
+
             let hiddenNumberR = Math.floor(Math.random() * (crystalHiddenNum.length));
             r = crystalHiddenNum[hiddenNumberR];
-        
+
             let hiddenNumberY = Math.floor(Math.random() * (crystalHiddenNum.length));
             y = crystalHiddenNum[hiddenNumberY];
-        
+
             let hiddenNumberB = Math.floor(Math.random() * (crystalHiddenNum.length));
             b = crystalHiddenNum[hiddenNumberB];
-        
+
+
+            //alert('ran x is ' + randomHiddenNum + ' ran g is ' + g + ' ran r is ' + r + ' ran y is ' + y + ' ran b is ' + b);
         });
 
-    }
-  
-    
-    let click = 0; {
 
-        let gc; {
+    }
+
+    let click = 0;
+    {
+        let gg; {
+
             $("#greenCrystal").click(() => {
                 click += g;
-                if (click) {
-                    document.getElementById("hiddenNum").innerHTML = click;
-                } else if (g > randomHiddenNum) {
-                    stop()
-                    document.getElementById("lose").innerHTML = "Sorry you lose, Please try again!"
-                } else (g === randomHiddenNum); {
-                    document.getElementById('win').innerHTML = "YEAH! YOU GOT IT!!!"
-                }
-            });           
-        }
 
-        let rc; {
-            $("#redCrystal").click(() => {
-                click += r;
-                if (click) {
+                if (click < randomHiddenNum) {
+
                     document.getElementById("hiddenNum").innerHTML = click;
-                } else if (r > randomHiddenNum) {
-                    stop()
-                    document.getElementById("lose").innerHTML = "Sorry you lose, Please try again!"
-                } else (r === randomHiddenNum); {
-                    document.getElementById('win').innerHTML = "YEAH! YOU GOT IT!!!"
+                } else if (click > randomHiddenNum) {
+                    //alert('current total is ' + g + ' > ranNum ' + randomHiddenNum + ' GAME OVER');
+                    document.getElementById("lose").innerHTML = "you lose";
+                } else if (click == randomHiddenNum) {
+                    //alert('current total is ' + g + ' = ranNum ' + randomHiddenNum + ' YOU GOT IT');
+                    document.getElementById("win").innerHTML = "you win";
                 }
             });
+
         }
 
-        let yc; {
-            $("#yellowCrystal").click(() => {
-                click += y;              
-                if (click) {
+        let rr; {
+
+            $("#redCrystal").click(() => {
+                click += r;
+
+                if (click < randomHiddenNum) {
+                    alert('current total is ' + r + ' < ranNum ' + randomHiddenNum);
                     document.getElementById("hiddenNum").innerHTML = click;
-                } else if (y > randomHiddenNum) {
-                    stop()
-                    document.getElementById("lose").innerHTML = "Sorry you lose, Please try again!"
-                } else (y === randomHiddenNum); {
-                    document.getElementById('win').innerHTML = "YEAH! YOU GOT IT!!!"
-                }           
-            }); 
+                } else if (click > randomHiddenNum) {
+                    //alert('current total is ' + r + ' > ranNum ' + randomHiddenNum + ' GAME OVER');
+                    document.getElementById("lose").innerHTML = "you lose";
+                } else if (click == randomHiddenNum) {
+                    //alert('current total is ' + r + ' = ranNum ' + randomHiddenNum + ' YOU GOT IT');
+                    document.getElementById("win").innerHTML = "you win";
+                }
+
+            });
+
         }
-    
-        let bc; {
+
+        let yy; {
+
+            $("#yellowCrystal").click(() => {
+                click += y;
+
+                if (click < randomHiddenNum) {
+                    alert('current total is ' + y + ' < ranNum ' + randomHiddenNum);
+                    document.getElementById("hiddenNum").innerHTML = click;
+                } else if (click > randomHiddenNum) {
+                    // alert('current total is ' + y + ' > ranNum ' + randomHiddenNum + ' GAME OVER');
+                    document.getElementById("lose").innerHTML = "you lose";
+                } else if (click == randomHiddenNum) {
+                    //alert('current total is ' + y + ' = ranNum ' + randomHiddenNum + ' YOU GOT IT');
+                    document.getElementById("win").innerHTML = "you win";
+                }
+
+            });
+
+
+        }
+
+        let bb; {
 
             $("#blueCrystal").click(() => {
                 click += b;
-                if (click) {
+
+                if (click < randomHiddenNum) {
+                    // alert('current total is ' + b + ' < ranNum ' + randomHiddenNum);
                     document.getElementById("hiddenNum").innerHTML = click;
-                } else if (b > randomHiddenNum) {
-                    stop()
-                    document.getElementById("lose").innerHTML = "Sorry you lose, Please try again!"
-                } else (b === randomHiddenNum); {
-                    document.getElementById('win').innerHTML = "YEAH! YOU GOT IT!!!"
-                }         
+                } else if (click > randomHiddenNum) {
+                    //alert('current total is ' + b + ' > ranNum ' + randomHiddenNum + ' GAME OVER');
+                    document.getElementById("lose").innerHTML = "you lose";
+                } else if (click == randomHiddenNum) {
+                    // alert('current total is ' + b + ' = ranNum ' + randomHiddenNum + ' YOU GOT IT');
+                    document.getElementById("win").innerHTML = "you win";
+                }
+
             });
+
+            randomNum()
         }
 
-
-randomNum()
-
-
     }
-
-
 
 
 
